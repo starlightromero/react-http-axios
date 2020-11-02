757cf80d0dc09b3fc96e77f55e47ca2333c65dae
Starlight Romero <starlightromero@gmail.com>
2020-11-01 18:06:49 -0800

    added axios instance

--- src/Containers/Blog/Blog.js
+++ src/Containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import axios from '../../axios'
 
 import Post from '../../Components/Post/Post'
 import FullPost from '../../Components/FullPost/FullPost'
