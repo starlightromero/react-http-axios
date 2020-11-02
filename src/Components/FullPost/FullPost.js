1f3c8bc7a0c630d171bea1d3bce212265f7f6a23
Starlight Romero <starlightromero@gmail.com>
2020-11-01 18:01:47 -0800

    set default axios configuration

--- src/Components/FullPost/FullPost.js
+++ src/Components/FullPost/FullPost.js
@@ -10,7 +10,7 @@ class FullPost extends Component {
   comonentDidUpdate () {
     if (this.props.id) {
       if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {        
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`).then(response => {
+        axios.get(`posts/${this.props.id}`).then(response => {
           this.setState({loadedPost: response.data})
         })
       }
@@ -18,7 +18,7 @@ class FullPost extends Component {
   }
 
   deletePostHandler = () => {
-    axios.delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`).then(response => {
+    axios.delete(`posts/${this.props.id}`).then(response => {
       console.log(response)
     })
   }
--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -11,7 +11,7 @@ class NewPost extends Component {
 
   postDataHandler = () => {
     const { title, body, author } = this.state
-    axios.post('https://jsonplaceholder.typicode.com/posts', {
+    axios.post('posts', {
       title,
       body,
       author
