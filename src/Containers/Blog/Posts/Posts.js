344b335f6c915d9795906bb1164a655e4e3b670a
Starlight Romero <starlightromero@gmail.com>
2020-11-03 13:00:42 -0800

    added Posts component

---
+++ src/Containers/Blog/Posts/Posts.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import classes from './Posts.module.css'
+import axios from '../../../axios'
+import Post from '../../../Components/Post/Post'
+
+class Posts extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount () {
+    axios.get('posts').then(response => {
+      const posts = response.data.slice(0, 4)
+      const updatedPosts = posts.map(post => {
+        return {...post, author: 'Starlight'}
+      })
+      this.setState({posts: updatedPosts})
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+
+  postSelectedHandler = id => {
+    this.setState({selectedPostId: id})
+  }
+  
+  render () {
+    let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+    if (!this.state.error) {      
+      posts = this.state.posts.map(post => {
+        return (
+          <Link to={`/${post.id}`} key={post.id}>
+            <Post
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)}
+            />
+          </Link>
+        )
+      })
+    }
+    
+    return (
+      <section className={classes.Posts}>
+        {posts}
+      </section>
+    )
+  }
+}
+
+export default Posts
