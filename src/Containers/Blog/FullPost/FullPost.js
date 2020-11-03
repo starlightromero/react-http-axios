5e52cad3c188c40291851dd61a6a5fb1a3566e05
Starlight Romero <starlightromero@gmail.com>
2020-11-03 13:26:20 -0800

    extracted route parameters

--- src/Containers/Blog/FullPost/FullPost.js
+++ src/Containers/Blog/FullPost/FullPost.js
@@ -7,10 +7,11 @@ class FullPost extends Component {
     loadedPost: null
   }
 
-  componentDidUpdate () {
-    if (this.props.id) {
+  componentDidMount () {
+    const id = this.props.match.params.id
+    if (id) {
       if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {        
-        axios.get(`posts/${this.props.id}`).then(response => {
+        axios.get(`posts/${id}`).then(response => {
           this.setState({loadedPost: response.data})
         })
       }
